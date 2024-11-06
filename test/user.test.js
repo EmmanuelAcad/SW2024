48beff9531ab7ddab546a1ed8d486d5067a50ddd
EmmanuelAcad <111817257+EmmanuelAcad@users.noreply.github.com>
2024-11-06 10:17:14 -0300

    wa

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app"); // Supondo que app.js exporte o seu app Express
+const app = require("../app");
 const chai = require("chai");
 const expect = chai.expect;
 
